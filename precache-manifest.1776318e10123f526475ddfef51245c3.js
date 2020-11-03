ffb1ec30f48ebd13cedcd02f5161256f36ef5635
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-11-03 22:11:17 +0700

    Updates

---
+++ precache-manifest.1776318e10123f526475ddfef51245c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "85d015bb62791d440e2f",
+    "url": "/neural-rhapsody-demo/static/css/main.9b1cd1d6.chunk.css"
+  },
+  {
+    "revision": "85d015bb62791d440e2f",
+    "url": "/neural-rhapsody-demo/static/js/main.85d015bb.chunk.js"
+  },
+  {
+    "revision": "d0bba6d1eb76f0560222",
+    "url": "/neural-rhapsody-demo/static/js/1.d0bba6d1.chunk.js"
+  },
+  {
+    "revision": "17a9b01bf38b45893b9c",
+    "url": "/neural-rhapsody-demo/static/js/runtime~main.17a9b01b.js"
+  },
+  {
+    "revision": "31b6daed8b7a5414c834d3462993da80",
+    "url": "/neural-rhapsody-demo/static/media/mozart.31b6daed.jpg"
+  },
+  {
+    "revision": "3aa357f32c9ee97aa434fa4179a74a26",
+    "url": "/neural-rhapsody-demo/static/media/chopin.3aa357f3.jpg"
+  },
+  {
+    "revision": "620f90005b88be080820995c007c7eca",
+    "url": "/neural-rhapsody-demo/static/media/beethoven.620f9000.jpg"
+  },
+  {
+    "revision": "ee2d02c40139498879e0fd078f815dc8",
+    "url": "/neural-rhapsody-demo/static/media/loader.ee2d02c4.gif"
+  },
+  {
+    "revision": "c24402375faa309496052832b7807321",
+    "url": "/neural-rhapsody-demo/index.html"
+  }
+];
